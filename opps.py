b65eaade0e1e288b3d9a6a53f98d4f7ee4927b9f
Gitmb56 <digib456@gmail.com>
2026-02-21 14:47:51 +0530

    oop new code

--- opps.py
+++ opps.py
@@ -1,24 +1,36 @@
-def bark(self):
-    print("Bhow Bhow")
-Dog = type(
-    "Dog",                   # class name
-    (),                      #parent classes
-    {
-        "legs":4,
-        "bark":bark
-    }
-)
+class BankAccount:
+    """
+    Docstring for BankAccount
+    Here are private some details and which method use Encapsulataion
+    and some techniques mangled
+    """
+    def __init__(self):
+        self.__name = " Mangal"                             # Mangaled name ( Private this name)
+        self.__balance = 0
+    
+    ###################################
+    #     I deposit amount this account
+    ###################################
+    
+    def deposit(self, amount:int):
+        if amount > 0:
+            self.__balance += amount
+            print(f"Deposited amount: {amount}")
+    
+    # withdraw amount
+    def withdraw(self, amount):
+        if self.__balance >= amount:
+            self.__balance -= amount
+            print(f"withdrawn amount: {amount}")
+        else:
+            print("Insufficient balance")
 
-d = Dog()
-print(d.legs)
-d.bark()
+    def show_details(self):
+        print(f"Account Holder name : {self.__name}")
+        print(f"Bank Balance: {self.__balance}")
 
-class Cat:
-    species = "Animal"
-
-    def bark(self):
-        print("Meow Meow")
-#print(Cat.__dict__)
-dd = Cat()
-print(dd.__dict__)
-
+ac1 = BankAccount()
+user_in = int(input("Enter your amount: "))
+ac1.deposit(user_in)
+ac1.withdraw(int(input("Enter your amount to withdraw: ")))
+print( f"Account Details: {ac1.show_details()}")
